2a942bbb02bd5393d7efeee818b36ce310234893
Christian Hinge <christianhinge@iMac-de-Christian.local>
2020-02-05 15:59:54 +0100

    wip

--- src/containers/UserProfile/MyRecipes.jsx
+++ src/containers/UserProfile/MyRecipes.jsx
@@ -1,26 +1,34 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect, useDispatch, useSelector } from 'react-redux';
 import { Link, useHistory } from 'react-router-dom';
-import { Button } from 'reactstrap';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import { asyncFetchDeleteRecipe, asyncFetchRecipesByUser } from '../../actions/recipes';
 import AddRecipeButton from '../InsertRecipe/AddRecipeButton';
 import PageHeader from '../../components/PageHeader';
 import RecipeCard from '../Recipes/RecipeCard';
 import './MyRecipes.scss';
 
-let MyRecipes = () => {
+let MyRecipes = (props) => {
   const content = useSelector(state => state);
   const dispatch = useDispatch();
   const history = useHistory();
   const { user, token } = content.user;
   const { recipes, loading, error } = content.recipes;
   const userRecipes = recipes.filter(recipe => recipe.user_id === user.id);
+  const { buttonLabel, className } = props;
+  const [modal, setModal] = useState(false);
+  const toggle = () => setModal(!modal);
 
   useEffect(() => { dispatch(asyncFetchRecipesByUser(token)) }, []);
   
   const goToMyProfile = () => history.push({pathname: 'my-account', state: {initialValues: user}});
   const modifyRecipe = recipe => history.push({pathname: 'create-recipe', state: recipe});
-  const deleteRecipe = (recipeId) => dispatch(asyncFetchDeleteRecipe(token, recipeId));
+  const deleteRecipe = (recipeId) => {
+    console.log(recipeId);
+    
+    // dispatch(asyncFetchDeleteRecipe(token, recipeId))
+    toggle();
+  };
   
   const header = {
     title: 'My Recipes',
@@ -45,17 +53,28 @@ let MyRecipes = () => {
           {userRecipes && userRecipes.length > 0 ? (
             userRecipes.map(userRecipe => (
               <li key={userRecipe.id}>
+              <Button onClick={() => console.log(userRecipe.id, userRecipe.title)} >What am I</Button>
                 <Link to={{pathname: `recipe/${userRecipe.id}`, state: {userRecipe}}}>
                   <RecipeCard recipe={userRecipe} />
                 </Link>
                   <div className="icons">
-                  <i className="fas fa-edit icon" 
+                  <i className="fas fa-edit icon-edit" 
                     onClick={() => modifyRecipe(userRecipe)} >
                   </i>
-                  <i className="fas fa-trash-alt icon"
-                    onClick={() => deleteRecipe(userRecipe.id)} >
+                  <i className="fas fa-trash-alt icon-del" // ??? ALWAYS CAPTURES LAST RECIPE IN ARRAY ???
+                    onClick={() => toggle()} >
                   </i>
                   </div>
+                  <Modal isOpen={modal} toggle={toggle} className={className} backdrop={true} >
+                    <ModalHeader toggle={toggle}>Confirm deleting this recipe</ModalHeader>
+                    <ModalBody>
+                      {userRecipe.title}
+                    </ModalBody>
+                    <ModalFooter>
+                      <Button color="danger" onClick={() => deleteRecipe(userRecipe.id)}>Delete</Button>{' '}
+                      <Button color="secondary" onClick={toggle}>Cancel</Button>
+                    </ModalFooter>
+                  </Modal>
               </li>
             ))
           ) : ''}
