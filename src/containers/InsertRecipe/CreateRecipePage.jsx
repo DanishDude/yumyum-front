7677fcc73a17a5787779d01319ee9b242ed95bfb
Christian Hinge <christianhinge@iMac-de-Christian.local>
2020-02-20 14:24:25 +0100

    form array

--- src/containers/InsertRecipe/CreateRecipePage.jsx
+++ src/containers/InsertRecipe/CreateRecipePage.jsx
@@ -8,14 +8,8 @@ const CreateRecipePage = (props) => {
   const { token } = props;
   const recipe = props.location.state;
   const dispatch = useDispatch();
-  const history = useHistory();
 
-  const addModifyRecipe = (token, values) => {
-    dispatch(asyncFetchAddModifyRecipe(token, values))
-    // history.push('/recipes')
-  };
-
-  
+  const addModifyRecipe = (token, values) => dispatch(asyncFetchAddModifyRecipe(token, values));
 
   return (
     <div className="CreateRecipePage">
--- src/containers/InsertRecipe/InsertRecipe.jsx
+++ src/containers/InsertRecipe/InsertRecipe.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from 'reactstrap';
 import { Field, reduxForm } from 'redux-form';
 import CancelAddRecipe from './CancelAddRecipe';
@@ -25,54 +25,77 @@ let InsertRecipe = (props) => {
     );
   };
 
+  const [index, setIndex] = useState(0);
+  const next = () => setIndex(index < recipeForm.length ? index + 1 : index);
+  console.log(index);
+  
+
+  const recipeForm = [
+    <div className="field">
+      <Field name="title" component="input" type="text" placeholder="Recipe title" />
+    </div>,
+
+    <div className="field">
+      <Field name="description" component="textarea" type="text" rows="4" wrap="hard"
+            placeholder="Share your storey... Where does this recipe come from? Why is it special?
+            Who else would love it?"
+      />
+    </div>,
+
+    <div className="field">
+      <Field name="ingredients" component="input" type="text"
+        placeholder="minced, beef, carrots, onions, herbs de provence"
+      />
+    </div>,
+
+    <div className="field">
+      <Field name="tag_list" component="input" type="text" placeholder="#parties #kids" />
+    </div>,
+
+    <div className="field">
+      <Field name="instructions" component="textarea" type="text" rows="4" wrap="soft"
+            placeholder="NB separate steps with | sign, e.g. Slice tomatoes | Throw in olives |
+            Sprinkle salt, pepper | Add a dash of olive oil | Top with freshly chopped basil"
+      />
+    </div>,
+
+    <div className="row">
+      <div className="field timer col-3">
+        <label htmlFor="prep_time">Prep time</label>
+        <Field name="prep_time" component="input" type="number"
+              min="0" max="999" step="1" />
+      </div>
+      <div className="field timer col-3">
+        <label htmlFor="cook_time">Cook time</label>
+        <Field name="cook_time" component="input" type="number"
+              min="0" max="999" step="1" />
+      </div>
+    </div>,
+
+    <div className="row">
+      <div className="field col-6">
+        <label htmlFor="image">Add a cover photo</label>
+        <Field name="image" component={FileInput} type="file" accept="image/png, image/jpeg"
+      />
+      </div>
+    </div>
+  ];
+
   return (
     <div className="InsertRecipe">
       <div className="content">
         <h3>Add your recipe</h3>
         <form onSubmit={handleSubmit}>
           <Field name="id" component="input" type="hidden" />
-          <div className="field">
-            <Field name="title" component="input" type="text" placeholder="Recipe title" />
-          </div>
-          <div className="field">
-            <Field name="description" component="textarea" type="text" rows="4" wrap="hard"
-                  placeholder="Share your storey... Where does this recipe come from? Why is it special?
-                  Who else would love it?"
-            />
-          </div>
-          <div className="field">
-            <Field name="ingredients" component="input" type="text"
-              placeholder="minced, beef, carrots, onions, herbs de provence"
-            />
-          </div>
-          <div className="field">
-            <Field name="tag_list" component="input" type="text" placeholder="#parties #kids" />
-          </div>
-          <div className="field">
-            <Field name="instructions" component="textarea" type="text" rows="4" wrap="soft"
-                  placeholder="NB separate steps with | sign, e.g. Slice tomatoes | Throw in olives |
-                  Sprinkle salt, pepper | Add a dash of olive oil | Top with freshly chopped basil"
-            />
-          </div>
-          <div className="row">
-            <div className="field timer col-3">
-              <label htmlFor="prep_time">Prep time</label>
-              <Field name="prep_time" component="input" type="number"
-                    min="0" max="999" step="1" />
-            </div>
-            <div className="field timer col-3">
-              <label htmlFor="cook_time">Cook time</label>
-              <Field name="cook_time" component="input" type="number"
-                    min="0" max="999" step="1" />
-            </div>
-            <div className="field col-6">
-              <label htmlFor="image">Add a cover photo</label>
-              <Field name="image" component={FileInput} type="file" accept="image/png, image/jpeg"
-            />
-            </div>
-          </div>
+          {recipeForm[index]}
+         
+          
           <div className="row">
             <div className="col-4"><CancelAddRecipe /></div>
+            <div>
+              <Button className="next" color="secondary" type="button" onClick={next}>Next</Button>
+              <p>{index}</p>
+            </div>
             <div className="field col-8">
               <Button className="submit-btn" color="primary" type="submit">Share</Button>
             </div>
