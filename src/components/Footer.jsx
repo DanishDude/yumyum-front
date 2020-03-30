63c6aa5d3605be57547759e34ff2481dca1423ed
Christian Hinge <christianhinge@iMac-de-Christian.local>
2020-03-30 17:30:54 +0200

    init footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.scss';
+
+const Footer = () => {
+  return (
+    <div className="Footer">
+      Hello, I am a Footer!
+    </div>
+  );
+};
+
+export default Footer;
