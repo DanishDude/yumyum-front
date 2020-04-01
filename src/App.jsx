055c240a1ef1fce84f652bd88b378ac993ec6db6
Christian Hinge <christianhinge@iMac-de-Christian.local>
2020-04-01 17:03:58 +0200

    rm about us

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import AboutUs from './components/AboutUs';
 import Contact from './components/Contact';
 import CreateRecipePage from './containers/InsertRecipe/CreateRecipePage';
 import EditMyProfile from './containers/UserProfile/EditMyProfile';
@@ -29,7 +28,6 @@ const App = () => {
           <Route exact path='/' component={Home} />
           <Route path = '/recipes' component={RecipesPage} />
           <Route path = '/recipe/:id' component={Recipe} />
-          <Route path = '/about-us' component={AboutUs} />
           <Route path = '/contact' component={Contact} />
           <Route path = '/login' component={LoginPage} />
           <Route path = '/signup' component={SignupPage} />
